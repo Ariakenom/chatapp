6402de0a3a166f0004a023d85f37233c36d0e6ee
Patrik Andersson <patrik.s.andersson@gmail.com>
2023-01-08 18:21:01 +0100

    remove chatlog. add local client changes. remove scrollbar. fix scroll to end. drop websocket server privileges

--- static/main.js
+++ static/main.js
@@ -2,24 +2,28 @@ window.addEventListener("DOMContentLoaded", () => {
     let server_chatlog = "";
     let client_chatcounter = 0;
     let server_chatcounter = 0;
+    let client_local_changes = "";
     const chatbox = document.getElementById('chatbox');
+    chatbox.value=""; // for some reason it keeps this over page reloads
     let host = "localhost"; 
     if (window.location.host !== "") {
         host = window.location.host;
-    }
+    };
     const websocket = new WebSocket(`ws://${host}:8001/`);
 
     chatbox.addEventListener('input', async event => {
         let changes = "";
         function undoChanges() {
-            chatbox.selectionStart = chatbox.value.length;
+            chatbox.setSelectionRange(chatbox.value.length,chatbox.value.length);
             chatbox.value = server_chatlog;
+            chatbox.scrollTop = chatbox.scrollHeight;
         };
         isInsert = type => ["insertText",  "insertCompositionText"].includes(type) || type.startsWith("insertFrom");
         // Adding in the middle is too hard
         if (chatbox.selectionStart !== chatbox.value.length) {
             undoChanges();
-        } else if (isInsert(event.inputType) && event.data !== null) {  // chrome sometimes has event.data=null here ???
+        } else 
+        if (isInsert(event.inputType) && event.data !== null) {  // chrome sometimes has event.data=null here ???
             changes += event.data;
         } else if (event.inputType === "insertLineBreak") {
             changes += "\n";
@@ -29,36 +33,51 @@ window.addEventListener("DOMContentLoaded", () => {
             undoChanges();
         }
         
-        if (changes) {
-            client_chatcounter += changes.length
+        if (changes !== "") {
+            client_local_changes += changes;
+            client_chatcounter += changes.length;
             websocket.send(changes);
         }
-    })
+    });
   
     websocket.onmessage = ({ data }) => {
-        if ("chatcounter" in data) {
-            server_chatcounter = data["chatcounter"]
+        const message = JSON.parse(data);
+        console.log("onmessage");
+        console.log(message);
+        if ("chatcounter" in message) {
+            server_chatcounter = message["chatcounter"];
         }
-        if ("chatlog" in data){
-            server_chatlog = data;
-            if (client_chatcounter === server_chatcounter){
-                chatbox.value = server_chatlog;
-                chatbox.selectionStart = chatbox.value.length;
-            }
+        if ("chatmessage" in message){
+            server_chatlog += message.chatmessage;
+            // .slice(-n) keeps the last n characters, so n should be length of the local changes
+            client_local_changes = client_local_changes.slice(client_local_changes.length+server_chatcounter-client_chatcounter);
+            chatbox.value = server_chatlog + client_local_changes;
+            chatbox.scrollTop = chatbox.scrollHeight;
         }
     };
 
     websocket.onclose = (event) => {
         console.log("onclose");
         console.log(event);
-        chatbox.disabled = true;
         chatbox.value += "\n:(\nerror\ndetails in console";
-    }
+        chatbox.setSelectionRange(chatbox.value.length, chatbox.value.length);
+        chatbox.disabled = true;
+    };
 
-    socket.onerror = (event) => {
-        console.log("onclose");
+    websocket.onerror = (event) => {
+        console.log("onerror");
         console.log(event);
-        chatbox.disabled = true;
         chatbox.value += "\n:(\nerror\ndetails in console";
-      };
+        chatbox.setSelectionRange(chatbox.value.length, chatbox.value.length);
+        chatbox.disabled = true;
+    };
+    
+    chatbox.onfocus = (event) => {
+        chatbox.setSelectionRange(chatbox.value.length, chatbox.value.length);
+    };
 });
+
+// Chrome: The focus event fires before the cursor is placed,
+// so set selection range doesnt work
+// Chrome: You don't get the paste data when a paste happens,
+// so I dont implement paste for chrome
--- websocket.py
+++ websocket.py
@@ -8,52 +8,59 @@ import os
 import json
 
 
-chatlog = ""
 open_sockets = set()
-chatlog_max_size = 1000
 
 
 async def main():
     loop = asyncio.get_running_loop()
     stop = loop.create_future()
     loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
-    host = "localhost"
+    loop.add_signal_handler(signal.SIGINT, stop.set_result, None)
+    host = ""
     port = int(os.environ.get("PORT", "8001"))
     async with websockets.serve(on_connection, host, port):
-        print("Serving.")
+        print(f"Serving host={host} port={port}.")
         await stop
+    print("\nClean shutdown.")
 
 
 async def on_connection(socket):
-    global chatlog, open_sockets, chatlog_max_size
+    global open_sockets
     chatcounter = 0  # counts characters from client that are in the chatlog
+    open_sockets.add(socket)
     print(f"Connection {socket}")
     print(f"Number of connections: {len(open_sockets)}")
-    await socket.send(chatlog)
-    open_sockets.add(socket)
     try:
         async for message in socket:
             print(f"Message from: {socket}")
             print(f"Message contains: {message!r}")
             chatcounter += len(message)
-            chatlog += message
-            if chatlog_max_size < len(chatlog):
-                chatlog = chatlog[-chatlog_max_size//2:]
-            asyncio.create_task(websocket.send(json.dumps({"chatcounter": chatcounter})))  # notify the client that it's changes are included
-            websockets.broadcast(open_sockets, json.dumps({"chatlog": chatlog}))
+            websockets.broadcast(open_sockets-{socket}, json.dumps({"chatmessage": message}))
+            asyncio.create_task(socket.send(json.dumps({"chatcounter": chatcounter, "chatmessage": message})))  # notify the client that it's changes are included
     except Exception as e:
         print(e)
     finally:
         print(f"Closed socket {socket}")
         open_sockets.remove(socket)
 
-try:
-    asyncio.run(main())
-except KeyboardInterrupt:
-        print("Quit because of KeyboardInterrupt.")
 
-"""
-Improvement:
-    when sending chatlog, include how much of the users input is in it
-    that way, when the users gets the chatlog, it can see if anything has been typed that isnt in the chatlog
-"""
+def drop_privileges(uid_name='nobody', gid_name='nogroup'):
+    if os.getuid() != 0:
+        # We're not root so, like, whatever dude
+        return
+    running_uid = pwd.getpwnam(uid_name).pw_uid
+    running_gid = grp.getgrnam(gid_name).gr_gid
+
+    # Remove group privileges
+    os.setgroups([])
+
+    os.setgid(running_gid)
+    os.setuid(running_uid)
+
+    # Ensure a very conservative umask
+    old_umask = os.umask(0o077)
+
+
+drop_privileges()
+asyncio.run(main())
+
